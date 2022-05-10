459b56c55d364016fefb2ea6fbddad0ddd5e7e78
EbiPenMan <karimi.gamedev@gmail.com>
2022-05-11 01:29:07 +0430

    code

--- Assets/ProGraphGroup/Projects/TruckWorld/Scripts/Runtime/PanelController/IntroPanelController.cs
+++ Assets/ProGraphGroup/Projects/TruckWorld/Scripts/Runtime/PanelController/IntroPanelController.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,16 +7,53 @@ namespace ProGraphGroup.Projects.TruckWorld
 {
     public class IntroPanelController : MonoBehaviour
     {
-        // Start is called before the first frame update
-        void Start()
+        public GameObject rootGo;
+        public int currentPageIndex = 0;
+        public List<GameObject> pageList;
+
+        public void Start()
         {
-        
+            GoToPage(currentPageIndex);
         }
 
-        // Update is called once per frame
-        void Update()
+        public void NextPage()
         {
-        
+            Debug.Log("NextPage");
+            if (currentPageIndex >= pageList.Count)
+                return;
+
+            GoToPage(currentPageIndex + 1);
+        }
+
+        public void PreviousPage()
+        {
+            Debug.Log("PreviousPage");
+            if (currentPageIndex == 0)
+                return;
+
+            GoToPage(currentPageIndex - 1);
+        }
+
+
+        private void GoToPage(int pageIndex)
+        {
+            Debug.Log("GoToPage");
+            pageList[currentPageIndex].SetActive(false);
+            currentPageIndex = pageIndex;
+            pageList[currentPageIndex].SetActive(true);
+        }
+
+        public void Show()
+        {
+            Debug.Log("Show");
+            rootGo.SetActive(true);
+    
+        }
+        public void Hide()
+        {
+            Debug.Log("Hide");
+            rootGo.SetActive(false);
+   
         }
     }
 }
