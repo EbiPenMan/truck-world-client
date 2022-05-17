d0f9d9a12fa3a6ca20d4a18b38623f881a90e336
EbiPenMan <karimi.gamedev@gmail.com>
2022-05-17 23:01:37 +0430

    controllers

---
+++ Assets/ProGraphGroup/General/Scripts/Runtime/Interfaces/IPlayerPref.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace ProGraphGroup.General.Interfaces
+{
+    public interface IInit<T> where T : class
+    {
+        public void Init(T data = null, Action<bool> onDone = null);
+    }
+}
